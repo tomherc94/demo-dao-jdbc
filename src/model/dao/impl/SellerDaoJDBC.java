8172f6a3f347b488624210472fe0068da4b8ba68
Tomas Herculano <tomasherculano@gmail.com>
2019-10-05 16:28:17 -0300

    DepartmentDao implements

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -85,10 +85,6 @@ public class SellerDaoJDBC implements SellerDao {
 		}finally{
 			DB.closeStatement(st);
 		}
-		
-		
-		
-
 	}
 
 	@Override
@@ -105,7 +101,6 @@ public class SellerDaoJDBC implements SellerDao {
 		}finally {
 			DB.closeStatement(st);
 		}
-
 	}
 
 	@Override
